50ffdd018f91fe03929fe6812bd4f243323962d1
KirillDemyanenko <demyanenkokg@gmail.com>
2024-12-05 11:49:03 +0300

    feat: implement sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,10 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return new Array(Math.max(arr1.length, arr2.length))
+    .fill(0)
+    .map((_, i) => (arr1.at(i) ?? 0) + (arr2.at(i) ?? 0));
 }
 
 /**
