717dd82ab6507cc41c1b8d1f1b5537364d30efa1
KirillDemyanenko <demyanenkokg@gmail.com>
2024-12-17 12:13:34 +0300

    feat: implement findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -479,8 +479,25 @@ function findCommonElements(/* arr1, arr2 */) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let result = 0;
+  let tmp = 0;
+  nums.map((value, index) => {
+    if (index > 0) {
+      if (value > nums[index - 1]) {
+        tmp += 1;
+      } else {
+        tmp = 1;
+      }
+    } else {
+      tmp = 1;
+    }
+    if (tmp > result) {
+      result = tmp;
+    }
+    return value;
+  });
+  return result;
 }
 
 /**
